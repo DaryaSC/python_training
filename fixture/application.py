decf7e7f47b4f51291bce17768ed0700825ccc9b
Zyryanova <zyryanova@svyazcom.ru>
2022-12-29 12:07:28 +0700

    task 4-6

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import CantactHelper
 
 
 class Application:
@@ -11,9 +12,13 @@ class Application:
         self.vars = {}
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = CantactHelper(self)
 
     def open_home_page(self):
         self.driver.get("http://localhost/addressbook/")
+
+    def return_to_home_page(self):
+        self.driver.find_element(By.LINK_TEXT, "home").click()
 
     def destroy(self):
         self.driver.quit()
