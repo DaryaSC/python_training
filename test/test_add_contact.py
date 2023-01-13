9d4651bbad03425e8f52c886834d2d3e8fab3047
Zyryanova <zyryanova@svyazcom.ru>
2023-01-13 11:21:15 +0700

    Task 15(Parameterization of tests)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,36 @@
 from model.contact import Contact
+import random
+import string
+import pytest
 
 
-def test_add_contact(app):
+def random_string(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(maxlen):
+    symbols = string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
+                    mobile="", work="", fax="", email="", email2="", email3="", homepage="", address2="", phone2="",
+                    notes="")] + [
+    Contact(firstname=random_string(10), middlename=random_string(10),
+            lastname=random_string(10), nickname=random_string(10),
+            title=random_string(10), company=random_string(10), address=random_string(10),
+            home=random_number(7), mobile=random_number(7), work=random_number(7), fax=random_number(7),
+            email=random_string(10), email2=random_string(10), email3=random_string(10),
+            homepage=random_string(10), address2=random_string(10), phone2=random_number(7),
+            notes=random_string(10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="fgh", middlename="hfgh", lastname="fgh", nickname="fhfg", title="hfg", company="fghfg",
-                address="fghfh", home="54", mobile="546", work="4864", fax="345343",
-                email="fdgf", email2="gsaf", email3="sdfh", homepage="gdsrg", address2="gdg", phone2="gdfg",
-                notes="dgdfg")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,25 @@
+import random
+import string
 from model.group import Group
+import pytest
 
 
-def test_add_group(app):
+def random_string(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string(10), header=random_string(20), footer=random_string(20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="dfg", header="fhf", footer="rde")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-#def test_add_empty_group(app):
-#   old_groups = app.group.get_group_list()
-#   app.group.create(Group(name="", header="", footer=""))
-#   new_groups = app.group.get_group_list()
-#   assert len(old_groups) + 1 == len(new_groups)
