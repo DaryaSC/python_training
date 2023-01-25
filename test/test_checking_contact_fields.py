f82694c0d5527769621853c5fc5f27d99eda5021
Zyryanova <zyryanova@svyazcom.ru>
2023-01-25 14:53:06 +0700

    Task 21(new tests for checking information about contacts on the main page)

--- test/test_checking_contact_fields.py
+++ test/test_checking_contact_fields.py
@@ -1,17 +1,20 @@
-from random import randrange
 import re
+from model.contact import Contact
 
 
-def test_checking_contact_fields(app):
+def test_checking_contact_fields(app, db):
     contacts_from_home_page = app.contact.get_contact_list()
-    index = randrange(len(contacts_from_home_page))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    contacts_from_db = db.get_contact_list()
+    sorted_contacts_from_home_page = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    sorted_contacts_from_db = sorted(contacts_from_db, key=Contact.id_or_max)
+    for i in range(len(contacts_from_db)):
+        assert sorted_contacts_from_home_page[i].lastname == sorted_contacts_from_db[i].lastname
+        assert sorted_contacts_from_home_page[i].firstname == sorted_contacts_from_db[i].firstname
+        assert sorted_contacts_from_home_page[i].address == sorted_contacts_from_db[i].address
+        assert sorted_contacts_from_home_page[i].all_phones_from_home_page == \
+               merge_phones_like_on_home_page(sorted_contacts_from_db[i])
+        assert sorted_contacts_from_home_page[i].all_emails_from_home_page == \
+               merge_emails_like_on_home_page(sorted_contacts_from_db[i])
 
 
 def clear_phone(s):
